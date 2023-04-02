9b4669bf1074919c002a07397ecf121010cf15ee
ahmadaliyul891 <ahmadaliyul891@gmail.com>
2023-04-02 08:23:53 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'ui/home_page.dart';
+//import 'column_widget.dart';
+//import 'row_widget.dart';
+//import 'baris_kolom.dart';
+//import '/ui/poli_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Klinik',
+      debugShowCheckedModeBanner: false,
+      //home: ColumnWidget(),
+      //home: RowWidget(),
+      //home: BarisKolom(),
+      //home: PoliPage(),
+      home: HomePage(),
+    );
+  }
+}
---
+++ lib/model/database.dart
@@ -0,0 +1,73 @@
+List<Map> pegawai = [
+  {
+    'id': 1,
+    'nip': '12211441',
+    'nama': 'Hamid Putrawan',
+    'tgllahir': '30 April 2000',
+    'nmrtlp': '085803511370',
+    'email': 'hamidputrawan1@gmail.com',
+    'password': '111',
+  },
+  {
+    'id': 2,
+    'nip': '12210974',
+    'nama': 'Fadila Chika Kusumarini',
+    'tgllahir': '27 Januari 2000',
+    'nmrtlp': '081233459237',
+    'email': 'fadilacikcik12@gmail.com',
+    'password': '222',
+  },
+  {
+    'id': 3,
+    'nip': '12211536',
+    'nama': 'Bendi Nur Alif',
+    'tgllahir': '16 Maret 1998',
+    'nmrtlp': '085292860995',
+    'email': 'bendi345@gmail.com',
+    'password': '333',
+  },
+  {
+    'id': 4,
+    'nip': '12210187',
+    'nama': 'Alfi Syahrin',
+    'tgllahir': '16 Oktober 1995',
+    'nmrtlp': '081214821516',
+    'email': 'alfisyahrin88@gmail.com',
+    'password': '444',
+  },
+];
+
+List<Map> pasien = [
+  {
+    'id': 1,
+    'noRm': '120',
+    'nama': 'Hasan Najib',
+    'tgllahir': '10 November 1997',
+    'nmrtlp': '085647918559',
+    'alamat': 'Klaten',
+  },
+  {
+    'id': 2,
+    'noRm': '125',
+    'nama': 'Roni Wiranto',
+    'tgllahir': '29 Maret 2000',
+    'nmrtlp': '085311311001',
+    'alamat': 'Karanganyar',
+  },
+  {
+    'id': 3,
+    'noRm': '198',
+    'nama': 'Eva Kristiana',
+    'tgllahir': '02 April 1989',
+    'nmrtlp': '085748661335',
+    'alamat': 'Ngawi',
+  },
+  {
+    'id': 4,
+    'noRm': '154',
+    'nama': 'Brigita Alasavista',
+    'tgllahir': '22 November 1990',
+    'nmrtlp': '081292701326',
+    'alamat': 'Wonogiri',
+  },
+];
